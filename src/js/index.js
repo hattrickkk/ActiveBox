376edafebb6415d00d4aac07e1897fcde3c149ab
hattrick <dasha.khitrick@gmail.com>
2022-12-31 13:51:40 +0300

    add all sections and media queries

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('input[type=button]').onclick = function() {
-		console.log(new Date())
-	}
-})
+// document.addEventListener("DOMContentLoaded", function(){
+// 	document.querySelector('input[type=button]').onclick = function() {
+// 		console.log(new Date())
+// 	}
+// })
+
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$(".header__burger, .header__menu").toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.menu__link').click(function (event) {
+		$(".header__burger, .header__menu").toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+new Swiper('.swiper', {
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	centeredSlides: true,
+	initialSlide: 1,
+	loop: true,
+	autoplay: true,
+	speed: 500,
+	autoHeight: true,
+});
